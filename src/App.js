cf899f3d999dd6d06ad50d8bdc73706359d9da5f
Dev2 Dev2 <DizicoreDev2@outlook.com>
2026-05-29 17:26:23 +0530

    small changes v1

--- src/App.js
+++ src/App.js
@@ -26,6 +26,6 @@ function App() {
       </Routes>
     </>
   );
-} 
+}
 
 export default App;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { useDispatch } from "react-redux";
 import { logout } from "../../redux/slices/authSlice";
